ed00e0222c09155deadf455abe93ac3e513f24d9
balbataeva <gbaltabaeva633@gmail.com>
2025-04-07 15:40:06 +0700

    Программа, выводgcc pr004.c -o pr004.out

---
+++ pr004.c
@@ -0,0 +1,25 @@
+//Программа, иллюстрирующая создание процесса-ребенка при помощи fork()
+#include <stdio.h>
+#include <sys/types>
+#include <unistd.h>
+#include <stdlib.h>
+int main() {
+	pid_t prid, parprid;
+	int a = 0, c;
+	prid = getpid();
+	parprid = getppid();
+	printf("До выполнения fork():\n");
+	printf("Ид. текущего процесса: %d\n", prid);
+	printf("Ид. родительского процесаа  %d\n",parprid);
+        printf("начальное значение переменной а: %d",а);	
+	c = fork();
+	a = a+1;
+	prid = getpid();
+	        parprid = getppid();
+		        printf("После выполнения fork():\n");
+			        printf("Ид. текущего процесса: %d\n", prid);
+				        printf("Ид. родительского процесаа  %d\n",parprid);
+					        printf("Значение переменной а: %d",а)
+
+	return 0;
+}
