4c915b6d8388643fe9e43f0f34829fbb847ad01c
balbataeva <gbaltabaeva633@gmail.com>
2025-04-29 16:52:24 +0700

    Изменение пользовательского контекста. Версия 2

--- pr007.c
+++ pr007.c
@@ -5,12 +5,30 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[], char *envp[]){
-	printf("Внутри программы pr007.out\n");
-	printf("Переключение пользовательского контекста на программу cat\n\n");
-
-	(void) execle("/bin/cat","/bin/cat", 0, envp);
-	printf("Ошибка при выполнении системного вызова exec\n");
+	int result;
+	pid_t pid, ppid;
+	pid = getpid();
+	ppid = getppid();
+	printf("Ид. текуущего процесса:%d, ид. родительского процесса:%d\n",pid, ppid);
+        printf("Выполняем fork()n");
+	result = fork();
+	if (result >0){
+		pid =  getpid();
+		ppid = getppid();
+	printf("Работает процесс-родитель\n");
+	printf("После запуска fork():\n");
+	printf("Ид.текущего процесса: %d, ид. родительского процесса:%d\n", pid, ppid);
+	printf("Родитель завершил работу\n");
+	}	
+	else if (result == 0){
+	printf("Заменяем пользовательский контекст процесса-ребенка\n");
+	(void)execle ("./pr002.out","/pr002.out", 0, envp);
+	printf("Ошибка при выполнении системного вызова exes\n");
 	exit(-1);
-	return 0;
-}	
-
+	}
+	else{
+		printf("Ошибка при ваыполнении fork()\n");
+		 exit (-1);
+	}
+	return 0;	
+}
