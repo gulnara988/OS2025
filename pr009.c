37bf184bc7685edf1fb712841946385fc1fab65e
balbataeva <gbaltabaeva633@gmail.com>
2025-05-05 15:42:50 +0700

    программа переброса информации через пайп внутри одног процеса

---
+++ pr009.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main() {
+	int fd[2];
+	size_t size;
+	char string[] = "3.124.2.24. OVSAINT.\n";
+	char resstring[20];
+	//Создание канала связи(pipe)
+	if (pipe(fd) < 0){
+		printf("Не удалось создать пайп\n");
+		exit(-1);
+	}
+	size = write(fd[1], string, 20);
+	if(size !=20) {
+		printf("Не удалось записать всю строку в пайп\n");
+		exit(-1);
+	}
+	size = read(fd[0], resstring, 20);
+	if(size !=20) {
+		printf("Не удалось прочитать всю строку в пайп\n");
+		exit(-1);
+	}
+	printf("Прочитанная строка:%s\n",resstring);
+	if(close(fd[1]) < 0) {
+       		printf("Не удалось закрыть выходной поток\n");
+	 	exit(-1);
+	}
+	if(close(fd[0]) < 0) {
+	       printf("Не удалось закрыть входной поток\n");
+	       exit(-1);
+	}
+	return 0;
+}
